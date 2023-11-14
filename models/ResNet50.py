e276259feb90b93cbff2be85c87dc198c54da459
Dan Avery <dan.github@danavery.com>
2023-11-14 11:54:07 -0800

    fixed transforms

--- models/ResNet50.py
+++ models/ResNet50.py
@@ -19,16 +19,16 @@ class ResNet50(nn.Module):
             "train": transforms.Compose(
                 [
                     transforms.Lambda(lambda x: x.unsqueeze(0).repeat(3, 1, 1)),  # Add channel dimension and repeat
-                    transforms.RandomResizedCrop(224, antialias=True),
-                    transforms.RandomHorizontalFlip(),
+                    transforms.Resize(224, antialias=True),  # Resize the shorter side to 224, maintaining aspect ratio
+                    transforms.Pad((0, 0, 224, 224), fill=0, padding_mode='constant'),  # Pad to make it 224x224
                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
                 ]
             ),
             "val": transforms.Compose(
                 [
                     transforms.Lambda(lambda x: x.unsqueeze(0).repeat(3, 1, 1)),  # Add channel dimension and repeat
-                    transforms.Resize(256, antialias=True),
-                    transforms.CenterCrop(224),
+                    transforms.Resize(224, antialias=True),  # Resize the shorter side to 224, maintaining aspect ratio
+                    transforms.Pad((0, 0, 224, 224), fill=0, padding_mode='constant'),
                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
                 ]
             ),
