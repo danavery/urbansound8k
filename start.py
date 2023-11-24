63a78a130bef5de0624fd4e980577a885837baec
Dan Avery <dan@danavery.com>
2023-11-23 19:17:21 -0800

    move main() to top of start.py

--- start.py
+++ start.py
@@ -5,6 +5,59 @@ from UrbanSoundPreprocessor import UrbanSoundPreprocessor
 from UrbanSoundTrainer import UrbanSoundTrainer
 
 
+def main():
+    print(f"{torch.cuda.is_available()=}")
+    print(f"{torch.backends.mps.is_available()=}")
+
+    # "hf" for hugging face dataset, "local" for dataset in source_dir
+    data_source = "hf"
+    source_dir = "/Users/davery/urbansound8k"
+    overwrite_specs = False
+    train_only = False
+    epochs_per_run = 1
+    wandb_run = False
+
+    model_type = "BasicCNN"
+    n_mels_list = [128]
+    n_fft_list = [1024]
+    chunk_timesteps = [112]
+    lr = 0.000005
+    mixup_alpha = 1  # if 1, then no mixup applied
+    batch_size = 256
+
+    print(f"Model: {model_type}")
+
+    for n_mels in n_mels_list:
+        for n_fft in n_fft_list:
+            for chunk_timestep in chunk_timesteps:
+                config = {
+                    "model_type": model_type,
+                    "n_mels": n_mels,
+                    "n_fft": n_fft,
+                    "chunk_timestep": chunk_timestep,
+                    "lr": lr,
+                    "mixup_alpha": mixup_alpha,
+                    "batch_size": batch_size,
+                }
+                print(config)
+                print("-" * 50)
+                if wandb_run:
+                    wandb.init(project="urbansound", config=config)
+                preprocessor, dataset_name = create_preprocessor(
+                    config, data_source, source_dir, overwrite_specs
+                )
+                try:
+                    trainer = create_trainer(
+                        preprocessor, config, batch_size, mixup_alpha, wandb_run
+                    )
+                    run_training(trainer, epochs_per_run, dataset_name, train_only)
+                    if wandb_run:
+                        wandb.finish()
+                except RuntimeError as e:
+                    print(f"error in {dataset_name}: {e}")
+                    continue
+
+
 def create_preprocessor(config, data_source, source_dir, overwrite_specs):
     dataset_name = f"n_mels-{config['n_mels']}-n_fft-{config['n_fft']}-chunk-{config['chunk_timestep']}"
     preprocessor = UrbanSoundPreprocessor(
@@ -47,62 +100,8 @@ def run_training(trainer, epochs_per_run, dataset_name, train_only):
         results = trainer.run(epochs=epochs_per_run, single_fold=1)
         print(f"\n{dataset_name}:")
         print(f"\t{results['train_loss']=:.5f} {results['train_acc']=:.2f}%", end="")
-        print(
-            f"{results['val_loss']=:.5f} {results['val_acc']=:.2f}% {results['grouped_acc']=:.2f}%"
-        )
-
-
-def main():
-    print(f"{torch.cuda.is_available()=}")
-    print(f"{torch.backends.mps.is_available()=}")
-
-    # "hf" for hugging face dataset, "local" for dataset in urbansound_dir
-    data_source = "hf"
-    source_dir = "/Users/davery/urbansound8k"
-    overwrite_specs = True
-    train_only = False
-    epochs_per_run = 1
-    wandb_run = False
-
-    model_type = "BasicCNN_2"
-    n_mels_list = [128]
-    n_fft_list = [1024]
-    chunk_timesteps = [112]
-    lr = 0.000005
-    mixup_alpha = 1  # if 1, then no mixup applied
-    batch_size = 256
-
-    print(f"Model: {model_type}")
-
-    for n_mels in n_mels_list:
-        for n_fft in n_fft_list:
-            for chunk_timestep in chunk_timesteps:
-                config = {
-                    "model_type": model_type,
-                    "n_mels": n_mels,
-                    "n_fft": n_fft,
-                    "chunk_timestep": chunk_timestep,
-                    "lr": lr,
-                    "mixup_alpha": mixup_alpha,
-                    "batch_size": batch_size,
-                }
-                print(config)
-                print("-" * 50)
-                if wandb_run:
-                    wandb.init(project="urbansound", config=config)
-                preprocessor, dataset_name = create_preprocessor(
-                    config, data_source, source_dir, overwrite_specs
-                )
-                try:
-                    trainer = create_trainer(
-                        preprocessor, config, batch_size, mixup_alpha, wandb_run
-                    )
-                    run_training(trainer, epochs_per_run, dataset_name, train_only)
-                    if wandb_run:
-                        wandb.finish()
-                except RuntimeError as e:
-                    print(f"error in {dataset_name}: {e}")
-                    continue
+        print(f"\t{results['val_loss']=:.5f} {results['val_acc']=:.2f}%")
+        print(f"\t{results['majority_acc']=:.2f}% {results['prob_avg_acc']=:.2f}%")
 
 
 if __name__ == "__main__":
